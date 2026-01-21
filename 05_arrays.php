2fec5acbe1d89cb0c07001a79ad2c97159b9c7c7
mastereps <efraim.gondraneos@gmail.com>
2026-01-21 15:07:38 +0800

    arrays

---
+++ 05_arrays.php
@@ -0,0 +1,32 @@
+<?php
+
+// Indexed Arrays
+// $fruits = ['Apple', 'Banana', 'Cherry'];
+// echo $fruits[0] . '<br>';
+// $fruits[3] = "Peach";
+
+// print_r($fruits);
+
+// echo '<pre>';
+// var_dump($fruits);
+// echo '</pre>';
+
+// Mixed Arrays
+// $mixedArray = [false, 'Apple', 14];
+
+// echo '<pre>';
+// var_dump($mixedArray);
+// echo '</pre>';
+
+// Associative Arrays Key Value pair
+$user = [
+    'name' => 'Epe',
+    'age' => 23,
+    'hobbies' => ['Coding', 'Playing Guitar']
+];
+
+echo $user['name'] . '<br>';
+
+echo '<pre>';
+var_dump($user);
+echo '</pre>';
