84249e6acd683d0728e2426f561f99b3ac32d427
mastereps <efraim.gondraneos@gmail.com>
2026-01-21 14:12:51 +0800

    echo statement

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body>
+    Hello World
+    <?php 
+      echo 'Hello PHP'
+    ?>
+    <!-- Notes
+    php -S localhost:8080 -> serve php without setting XAMPP
+    php .\<filename>
+    php .\index.php 
+    -->
+  </body>
+</html>
